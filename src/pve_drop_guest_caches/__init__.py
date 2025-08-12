6e104f8183a4e434a54b4be70a469a8c55ea5c78
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-08-12 12:35:40 +0900

    feat(app): Add the root endpoint

--- src/pve_drop_guest_caches/__init__.py
+++ src/pve_drop_guest_caches/__init__.py
@@ -1,2 +1,1 @@
-def hello() -> str:
-    return "Hello from pve-drop-guest-caches!"
+from .app import app
---
+++ src/pve_drop_guest_caches/__main__.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("run with\nuv run hypercorn pve_drop_guest_caches:app --bind 0.0.0.0:8081 --reload")
