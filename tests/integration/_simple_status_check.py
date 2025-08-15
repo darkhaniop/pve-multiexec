b95020872f4c1c0d0a7318381148f84912a4c464
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-08-15 11:48:21 +0900

    fix(pveapi2): fix request status

---
+++ tests/integration/_simple_status_check.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+import proxmoxer
+from dotenv import load_dotenv
+from proxmoxer import ProxmoxAPI
+
+
+from pve_drop_guest_caches.pve.api_initializer import create_proxmox_api
+
+
+load_dotenv()
+
+# logging.basicConfig()
+# proxmoxer.logger.setLevel(logging.DEBUG)
+
+
+def sent_request(proxmox_api: ProxmoxAPI, node: str) -> None:
+    return proxmox_api.nodes(node).status.get()
+
+
+if __name__ == "__main__":
+    print("quick status check")
+
+    proxmox_api = create_proxmox_api()
+    response = sent_request(proxmox_api, os.getenv("MY_PVE_NODE", ""))
+    print("response:\n", response)
