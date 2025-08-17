ddb2866b8b1e74066afa5fde75496a22e3eff02a
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-08-17 11:27:21 +0900

    feat(app): add routes /nodes and /nodes/{node}/vms

--- src/pve_drop_guest_caches/app.py
+++ src/pve_drop_guest_caches/app.py
@@ -1,4 +1,9 @@
+from typing import Annotated
+import asyncio
 from fastapi import FastAPI
+
+from .pve.api_initializer import create_proxmox_api
+from .pve.models import PveNode, PveQemuVm
 
 
 app = FastAPI()
@@ -7,3 +12,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/nodes")
+async def get_nodes() -> list[PveNode]:
+    def _get_nodes_in_worker():
+        pve_api = create_proxmox_api()
+        return pve_api.nodes.get()
+
+    nodes = await asyncio.to_thread(_get_nodes_in_worker)
+    return nodes
+
+
+@app.get("/nodes/{node}/vms", responses={404: {"description": "Unavailable node."}})
+async def get_node_vms(node: Annotated[str, "The cluster node name."]) -> list[PveQemuVm]:
+    def _get_node_qemu_in_worker():
+        pve_api = create_proxmox_api()
+        return pve_api.nodes(node).qemu.get()
+
+    node_vms = await asyncio.to_thread(_get_node_qemu_in_worker)
+    return node_vms
---
+++ src/pve_drop_guest_caches/pve/models.py
@@ -0,0 +1,55 @@
+from typing import Annotated, Literal
+from pydantic import BaseModel, Field
+
+class PveNode(BaseModel):
+    node: Annotated[str, Field(min_length=1, description="The cluster node name.")]
+    status: Annotated[Literal["unknown", "online", "offline"], Field(description="Node status.")]
+
+    cpu: Annotated[float | None, Field(default=None, description="CPU utilization.")]
+    level: Annotated[str | None, Field(default=None, description="Support level.")]
+    maxcpu: Annotated[int | None, Field(default=None, description="Number of available CPUs.")]
+    maxmem: Annotated[int | None, Field(default=None, description="Number of available memory in bytes.")]
+    mem: Annotated[int | None, Field(default=None, description="Used memory in bytes.")]
+    ssl_fingerprint: Annotated[str | None, Field(default=None, description="The SSL fingerprint for the node certificate.")]
+    uptime: Annotated[int | None, Field(default=None, description="Node uptime in seconds.")]
+    
+#   {
+#     "cpu": 0,
+#     "maxdisk": 8589934592,
+#     "serial": 1,
+#     "diskread": 0,
+#     "disk": 0,
+#     "mem": 0,
+#     "diskwrite": 0,
+#     "maxmem": 4294967296,
+#     "vmid": 220000220,
+#     "netin": 0,
+#     "cpus": 4,
+#     "netout": 0,
+#     "status": "stopped",
+#     "uptime": 0,
+#     "name": "experiment220"
+#   }
+class PveQemuVm(BaseModel):
+    status: Annotated[Literal["stopped", "running"], Field(description="QEMU process status.")]
+    vmid: Annotated[int, Field(ge=100, le=999999999, description="The (unique) ID of the VM.")]
+
+    cpu: Annotated[float | None, Field(default=None, description="Current CPU usage.")]
+    cpus: Annotated[float | None, Field(default=None, description="Maximum usable CPUs.")]
+    diskread: Annotated[int | None, Field(default=None, description="The amount of bytes the guest read from it's block devices since the guest was started. (Note: This info is not available for all storage types.)")]
+    diskwrite: Annotated[int | None, Field(default=None, description="The amount of bytes the guest wrote from it's block devices since the guest was started. (Note: This info is not available for all storage types.)")]
+    lock: Annotated[str | None, Field(default=None, description="The current config lock, if any.")]
+    maxdisk: Annotated[int | None, Field(default=None, description="Root disk size in bytes.")]
+    maxmem: Annotated[int | None, Field(default=None, description="Maximum memory in bytes.")]
+    mem: Annotated[int | None, Field(default=None, description="Currently used memory in bytes.")]
+    name: Annotated[str | None, Field(default=None, description="VM (host)name.")]
+    netin: Annotated[int | None, Field(default=None, description="The amount of traffic in bytes that was sent to the guest over the network since it was started.")]
+    netout: Annotated[float | None, Field(default=None, description="The amount of traffic in bytes that was sent from the guest over the network since it was started.")]
+    pid: Annotated[float | None, Field(default=None, description="PID of the QEMU process, if the VM is running.")]
+    qmpstatus: Annotated[float | None, Field(default=None, description="VM run state from the 'query-status' QMP monitor command.")]
+    running_machine: Annotated[float | None, Field(default=None, alias="running-machine", description="The currently running machine type (if running).")]
+    running_qemu: Annotated[float | None, Field(default=None, alias="running-qemu", description="The QEMU version the VM is currently using (if running).")]
+    serial: Annotated[bool | None, Field(default=None, description="Guest has serial device configured.")]
+    tags: Annotated[str | None, Field(default=None, description="The current configured tags, if any")]
+    template: Annotated[bool, Field(default=False, description="Determines if the guest is a template.")]
+    uptime: Annotated[int | None, Field(default=None, description="Uptime in seconds.")]
