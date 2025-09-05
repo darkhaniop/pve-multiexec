1a51321139cf92dc82df4c38f38eecbe1113d7fb
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-09-05 20:15:39 +0900

    chore(formatting): run basic formatting

--- src/pve_drop_guest_caches/__main__.py
+++ src/pve_drop_guest_caches/__main__.py
@@ -1,2 +1,4 @@
 if __name__ == "__main__":
-    print("run with\nuv run hypercorn pve_drop_guest_caches:app --bind 0.0.0.0:8081 --reload")
+    print(
+        "run with\nuv run hypercorn pve_drop_guest_caches:app --bind 0.0.0.0:8081 --reload"
+    )
