93f711838661a72b29ee8aa06e64c5da262b5922
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-09-12 13:28:51 +0900

    feat(api): implement routes /cmd_template

--- src/pve_drop_guest_caches/api_cmd_templates/router.py
+++ src/pve_drop_guest_caches/api_cmd_templates/router.py
@@ -1,43 +1,68 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
+
+from pve_drop_guest_caches.db import SessionDep
 
 from .models import CmdTemplate, CmdTemplateBase
 
 router = APIRouter()
 
 
-@router.get("/")
-async def get_cmd_templates():
-    return []
+@router.get("/", response_model=list[CmdTemplate])
+async def get_cmd_templates(session: SessionDep):
+    cmd_templates = session.exec(select(CmdTemplate)).all()
+    return cmd_templates
 
 
-@router.post("/")
-async def create_cmd_template(cmd_template: CmdTemplateBase) -> CmdTemplate:
+@router.post("/", response_model=CmdTemplate)
+async def create_cmd_template(
+    session: SessionDep, cmd_template: CmdTemplateBase
+) -> CmdTemplate:
     """Create a new cmd template"""
 
     db_cmd_template = CmdTemplate.model_validate(cmd_template)
+    session.add(db_cmd_template)
+    session.commit()
+    session.refresh(db_cmd_template)
     return db_cmd_template
 
 
 @router.get("/{cmd_template_id}")
-async def read_cmd_template(cmd_template_id: int) -> CmdTemplate:
+async def read_cmd_template(session: SessionDep, cmd_template_id: int) -> CmdTemplate:
     """Read an cmd template by id"""
 
-    db_cmd_template = CmdTemplate.model_validate({})
+    db_cmd_template = session.get(CmdTemplate, cmd_template_id)
+    if not db_cmd_template:
+        raise HTTPException(status_code=404, detail="CmdTemplate not found")
     return db_cmd_template
 
 
 @router.put("/{cmd_template_id}")
 async def update_cmd_template(
-    cmd_template_id: int, cmd_template: CmdTemplateBase
+    session: SessionDep, cmd_template_id: int, cmd_template: CmdTemplateBase
 ) -> CmdTemplate:
     """Update an existing cmd template"""
 
-    db_cmd_template = CmdTemplate.model_validate(cmd_template)
+    db_cmd_template = session.get(CmdTemplate, cmd_template_id)
+    if not db_cmd_template:
+        raise HTTPException(status_code=404, detail="CmdTemplate not found")
+
+    cmd_template_data = cmd_template.model_dump()
+    db_cmd_template.sqlmodel_update(cmd_template_data)
+    session.commit()
+    session.refresh(db_cmd_template)
     return db_cmd_template
 
 
 @router.delete("/{cmd_template_id}")
-async def delete_cmd_template(cmd_template_id: int):
+async def delete_cmd_template(session: SessionDep, cmd_template_id: int):
     """Delete an cmd template by id"""
 
-    return
+    db_cmd_template = session.get(CmdTemplate, cmd_template_id)
+    if not db_cmd_template:
+        raise HTTPException(status_code=404, detail="CmdTemplate not found")
+
+    session.delete(db_cmd_template)
+    session.commit()
+
+    return {"status": "deleted", "id": cmd_template_id}
--- src/pve_drop_guest_caches/db.py
+++ src/pve_drop_guest_caches/db.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy import Engine
 from sqlmodel import Session, SQLModel, create_engine
 
@@ -24,3 +27,6 @@ def db_init():
 def get_session():
     with Session(_LocalState.engine) as session:
         yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
